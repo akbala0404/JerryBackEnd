3b982936682d9f5c98c1624660dacd713231dcfe
koffer0-0 <68071028+Koffer0-0@users.noreply.github.com>
2023-09-23 23:45:58 +0600

    Update router_send_prompt.py

--- app/map/router/router_send_prompt.py
+++ app/map/router/router_send_prompt.py
@@ -1,7 +1,9 @@
+from array import array
+
 from fastapi import APIRouter, Depends, status, HTTPException
 from app.utils import AppModel, BaseModel
 from pydantic import Field
-from typing import Any
+from typing import Any, List, Dict
 from ..service import Service, get_service
 from . import router
 import requests
@@ -18,6 +20,22 @@ class RouteRequest(BaseModel):
 class RouteResponse(BaseModel):
     route: str
 
+class AutocompleteRequest(BaseModel):
+    input: str
+    language: str = "en"
+
+class FindPlaceRequest(BaseModel):
+    input: str
+    language: str = "en"
+
+class Place(BaseModel):
+    type: str
+    name: str
+    address: str = ""
+    description: str = ""
+
+class PlacesResponse(BaseModel):
+    data: str
 
 @router.post("/route", response_model=RouteResponse)
 def get_route(request: RouteRequest) -> RouteResponse:
@@ -26,11 +44,39 @@ def get_route(request: RouteRequest) -> RouteResponse:
     if not route_data:
         raise HTTPException(status_code=400, detail="Could not fetch route")
 
-    # You can extract and format the desired data from route_data as needed
-    # For simplicity, we'll return the 'summary' of the route.
-    return RouteResponse(route=route_data["summary"])
+    return RouteResponse(route=json.dumps(route_data))
+
+
+@router.post("/autocomplete", response_model=PlacesResponse)
+def autocomplete(request: AutocompleteRequest):
+    autocomplete_data = get_google_maps_autocomplete(request.input, request.language)
+
+    # For simplicity, let's assume that the autocomplete data gives us a list of names.
+    # You might need to adapt this extraction depending on the actual structure of the returned data.
+    names = [prediction['description'] for prediction in autocomplete_data.get('predictions', [])]
+
+    # For your format, you probably need to use the name to find the type, address, etc.
+    # This is a mocked way to generate the response you provided:
+    places = [Place(type="unknown", name=name) for name in
+              names]  # Here, "unknown" is just a placeholder. You'd need a way to map names to types.
+    return PlacesResponse(places=json.dumps(autocomplete_data))
+
+
+@router.post("/findplace", response_model=PlacesResponse)
+def find_place(request: FindPlaceRequest):
+    place_data = get_google_maps_findplace(request.input, request.language)
+
+    # You'd extract necessary info from place_data, but for this mock, let's assume you get a single place name.
+    # Again, adapt this based on the actual structure of the returned data.
+    name = place_data.get('candidates', [{}])[0].get('name', "")
+
+    # Construct the Place based on the found name.
+    place = Place(type="unknown", name=name)  # "unknown" is a placeholder again.
+    return PlacesResponse(places=json.dumps(place_data))
+
 
 def get_google_maps_route(origin: str, destination: str, mode: str) -> dict:
+    GOOGLE_MAPS_API_KEY = "AIzaSyDOtiS_ckxQn7JoyhUjLdasDTU9iL4F2Zc"
     base_url = "https://maps.googleapis.com/maps/api/directions/json?"
     params = {
         "origin": origin,
@@ -45,4 +91,30 @@ def get_google_maps_route(origin: str, destination: str, mode: str) -> dict:
         return None
 
     return data["routes"][0]
+
+def get_google_maps_autocomplete(input: str, language: str = "en") -> dict:
+    GOOGLE_MAPS_API_KEY = "AIzaSyDOtiS_ckxQn7JoyhUjLdasDTU9iL4F2Zc"
+    base_url = "https://maps.googleapis.com/maps/api/place/queryautocomplete/json?"
+    params = {
+        "input": input,
+        "language": language,
+        "key": GOOGLE_MAPS_API_KEY
+    }
+
+    response = requests.get(base_url, params=params)
+    return response.json()
+
+def get_google_maps_findplace(input: str, language: str = "en") -> dict:
+    GOOGLE_MAPS_API_KEY = "AIzaSyDOtiS_ckxQn7JoyhUjLdasDTU9iL4F2Zc"
+    base_url = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?"
+    params = {
+        "input": input,
+        "inputtype": "textquery",
+        "language": language,
+        "key": GOOGLE_MAPS_API_KEY
+    }
+
+    response = requests.get(base_url, params=params)
+    return response.json()
+
 
